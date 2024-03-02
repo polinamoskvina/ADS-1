523ef5855ff79ced05ebc32b93d392bf9ac6f5c8
polinamoskvina <160390879+polinamoskvina@users.noreply.github.com>
2024-03-02 15:48:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,67 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
+#include <iostream>
+#include <cmath>
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	if (value <= 1) {
+		return false;
+	}
+	for (uint64_t i = 2; i <= sqrt(value); i++) {
+		if (value % i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	uint64_t num = 2;
+	uint64_t count = 0;
+
+	while (count < n) {
+		if (checkPrime(num)) {
+			count++;
+		}
+		num++;
+	}
+
+	return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	uint64_t num = value + 1;
+
+	while (!checkPrime(num)) {
+		num++;
+	}
+
+	return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+
+	for (uint64_t i = 2; i < hbound; i++) {
+		if (checkPrime(i)) {
+			sum += i;
+		}
+	}
+
+	return sum;
 }
+int main() {
+	uint64_t number; //вводимое на проверку число
+	std::cin >> number;
+	uint64_t n; //ряд числа
+	std::cin >> n;
+	std::cout << "Is " << number << " prime? " << std::boolalpha << checkPrime(number) << std::endl;
+	std::cout << "The " <<  n << " th prime number is: " << nPrime(n) << std::endl;
+	std::cout << "Next prime number after " << number << " is: " << nextPrime(number) << std::endl;
+	std::cout << "Sum of all prime numbers up to " << number << " is: " << sumPrime(number) << std::endl;
+
+	return 0;
+}
+
